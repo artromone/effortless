1d950ba33bb7a48f1c4045cc59e0ee44e922b4bc
Artem Romanovich <art.rom.one@gmail.com>
2024-06-07 21:29:14 +0300

    only one app instance

--- main.cpp
+++ main.cpp
@@ -1,9 +1,52 @@
-#include <QGuiApplication>
+#include <QApplication>
 #include <QQmlApplicationEngine>
+#include <QSystemSemaphore>
+#include <QSharedMemory>
+#include <QMessageBox>
 
 int main(int argc, char *argv[])
 {
-    QGuiApplication app(argc, argv);
+    QApplication app(argc, argv);
+
+
+    QSystemSemaphore semaphore("<uniq id>", 1);  // создаём семафор
+    semaphore.acquire(); // Поднимаем семафор, запрещая другим экземплярам работать с разделяемой памятью
+
+#ifndef Q_OS_WIN32
+    // в linux/unix разделяемая память не освобождается при аварийном завершении приложения,
+    // поэтому необходимо избавиться от данного мусора
+    QSharedMemory nix_fix_shared_memory("<uniq id 2>");
+    if (nix_fix_shared_memory.attach())
+    {
+        nix_fix_shared_memory.detach();
+    }
+#endif
+
+    QSharedMemory sharedMemory("<uniq id 2>");  // Создаём экземпляр разделяемой памяти
+    bool is_running;            // переменную для проверки ууже запущенного приложения
+    if (sharedMemory.attach())
+    {                           // пытаемся присоединить экземпляр разделяемой памяти
+        // к уже существующему сегменту
+        is_running = true;      // Если успешно, то определяем, что уже есть запущенный экземпляр
+    }
+    else
+    {
+        sharedMemory.create(1); // В противном случае выделяем 1 байт памяти
+        is_running = false;     // И определяем, что других экземпляров не запущено
+    }
+    semaphore.release();        // Опускаем семафор
+
+    // Если уже запущен один экземпляр приложения, то сообщаем об этом пользователю
+    // и завершаем работу текущего экземпляра приложения
+    if (is_running)
+    {
+        QMessageBox msgBox;
+        msgBox.setIcon(QMessageBox::Warning);
+        msgBox.setText(QObject::tr("Приложение уже запущено.\n"
+                                   "Вы можете запустить только один экземпляр приложения."));
+        msgBox.exec();
+        return 1;
+    }
 
     QQmlApplicationEngine engine;
     const QUrl url(QStringLiteral("qrc:/effortless/Main.qml"));
